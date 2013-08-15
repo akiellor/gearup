3b7a089d81aacc9c88783d01e6f9ad7162a452e0
Andrew Kiellor <akiellor@gmail.com>
2013-08-15 09:12:29 -0700

    Added gzip support.

--- lib/gearup/server.rb
+++ lib/gearup/server.rb
@@ -8,7 +8,34 @@ module Gearup
     end
 
     def start
-      Rack::Handler::WEBrick.run(@env, :Port => @port)
+      Rack::Handler::WEBrick.run(app, :Port => @port)
+    end
+
+    private
+
+    def app
+      env = @env
+      Rack::Builder.new do
+        use Rack::Deflater
+        use GzRewrite
+
+        run env
+      end
+    end
+
+    class GzRewrite
+      def initialize app
+        @app = app
+      end
+
+      def call env
+        if env["PATH_INFO"] =~ /\.gz$/
+          env["PATH_INFO"] = env["PATH_INFO"].sub(/\.gz$/, "")
+          env["REQUEST_PATH"] = env["REQUEST_PATH"].sub(/\.gz$/, "")
+          env["REQUEST_URI"] = env["REQUEST_URI"].sub(/\.gz$/, "")
+        end
+        @app.call(env)
+      end
     end
   end
 end
